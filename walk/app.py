d7f6a1c800a47ad1a71847c88ac53dc30721ada5
Brian <brian0425@gmail.com>
2020-10-21 00:14:41 -0500

    trying to fix err

--- walk/app.py
+++ walk/app.py
@@ -1,3 +1,4 @@
+# add code fpr flask app object
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ import re
 
 
 
-
+# set route for user navigation
 @app.route('/')
 
 # define app function
@@ -24,7 +25,7 @@ def index():
     number = 20
 
     # move through list
-    search = index
+    search = UIC
     article = []
     results = 100 # valid options 10, 20, 30, 40, 50, and 100
     page = requests.get(f"https://www.google.com/search?q={search}&num={results}")
@@ -77,6 +78,39 @@ def UIC():
 
 
 
+@app.route('/usa')
+
+# define app function
+def usa():
+    # set up list
+    gallery = "gallery&400"
+    UIC = 'UIC'
+    chicago = 'chicago'
+    usa = 'usa'
+
+    # get number
+    number = 20
+
+    # move through list
+    search = usa
+    article = []
+    results = 100 # valid options 10, 20, 30, 40, 50, and 100
+    page = requests.get(f"https://www.google.com/search?q={search}&num={results}")
+    soup = BeautifulSoup(page.content, "html.parser")
+    links = soup.findAll("a")
+    for link in links :
+        link_href = link.get('href')
+        if "url?q=" in link_href and not "webcache" in link_href:
+            article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
+
+    page = requests.get(f'{article[number]}')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    text = (soup.text)
+
+    return render_template("usa.html", text = text)
+
+
+
 
 @app.route('/chicago')
 
@@ -108,37 +142,3 @@ def chicago():
     text = (soup.text)
 
     return render_template("chicago.html", text = text)
-
-
-
-
-@app.route('/uic')
-
-# define app function
-def usa():
-    # set up list
-    gallery = "gallery&400"
-    UIC = 'UIC'
-    chicago = 'chicago'
-    usa = 'usa'
-
-    # get number
-    number = 20
-
-    # move through list
-    search = usa
-    article = []
-    results = 100 # valid options 10, 20, 30, 40, 50, and 100
-    page = requests.get(f"https://www.google.com/search?q={search}&num={results}")
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.findAll("a")
-    for link in links :
-        link_href = link.get('href')
-        if "url?q=" in link_href and not "webcache" in link_href:
-            article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
-
-    page = requests.get(f'{article[number]}')
-    soup = BeautifulSoup(page.text, 'html.parser')
-    text = (soup.text)
-
-    return render_template("usa.html", text = text)
